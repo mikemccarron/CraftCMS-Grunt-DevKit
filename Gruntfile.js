1b9f023e8779d29abce19ebb06b18da03f968bdd
Mike McCarron <mikemccarron@1trickpony.com>
2014-07-09 11:42:41 -0400

    Added grunt-concurrent to loadNPMTasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -163,6 +163,7 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-bower-task');
+	grunt.loadNpmTasks('grunt-concurrent');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
